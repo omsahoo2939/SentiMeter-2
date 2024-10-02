94acef7fa90a60f4883b324e1a8b0d651dfc912a
Usman Jalil <alice@webage.com>
2024-10-02 10:14:37 -0400

    react

---
+++ react/gp2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
