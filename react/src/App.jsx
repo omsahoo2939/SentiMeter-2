ca0b3221dce3ccb299ee905f95179398e7ed38d4
Gauri Sarin <alice@webage.com>
2024-10-02 13:41:11 -0400

    css

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,13 +5,29 @@ import {
   Routes,
   Link
 } from "react-router-dom";
+
 import Characters from './components/Characters';
 import Character from './components/Character';
+import './App.css'; 
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="title">Star Wars API</h1>
+      <nav className="nav">
+        <ul>
+          <li><Link to="/">Characters</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <div>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Characters />} />
           <Route path="/characters/:id" element={<Character />} />
