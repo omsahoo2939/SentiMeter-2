7b5daae75f2387e9c7e4d5a3836ed07d3763898a
gaurisarin <saringauri@gmail.com>
2024-10-11 12:33:48 -0400

    messs

--- react/src/components/FeedbackForm.jsx
+++ react/src/components/FeedbackForm.jsx
@@ -48,7 +48,7 @@ const FormGroup = styled.div`
 
 const Label = styled.label`
   font-size: 1.2rem;
-  color: rgba(255, 255, 255, 0.9);
+  color: #fff;
   margin-bottom: 0.5rem;
   display: block;
 `;
